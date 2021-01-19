e0f8c49d2d27d48af23f539eeb71240495615efb
Lukas Reschke <lukas@statuscode.ch>
2021-01-19 10:32:15 +0000

    Adjust build file for newest hugo release

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+wget https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_0.80.0_Linux-64bit.tar.gz -O /tmp/hugo.tar.gz
+tar -C /tmp/ -xvf /tmp/hugo.tar.gz
 cd themes/gatekeeper/assets/scss/ && npm install
-cd ../../../../ && hugo
+cd ../../../../ && /tmp/hugo
